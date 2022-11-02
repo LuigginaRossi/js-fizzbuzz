5f3def6f7c9002a450815c58fc0f012e1b472617
LuigginaRossi <luigginavaldivia@gmail.com>
2022-11-02 18:47:07 +0100

    stampato nella pagina i valori richiesti

--- js/file.js
+++ js/file.js
@@ -1,73 +1,35 @@
 const containerBuzzEl = document.getElementById( "container-buzz" );
 
-console.log( containerBuzzEl );
 
-for ( let i = 0; i <= 100; i++ ) {
+for ( let i = 1; i <= 100; i++ ) {
 
     console.log(i);
+
     containerBuzzEl.innerHTML += `<div class="square-container"> ${i} </div>`;
-    console.log(containerBuzzEl);
 
-    //     TENTATIVO1=> FAILED
-   
-    // if ( i % 3 === 0 ) {
-
-    //     console.log( "Fizz" );
-
-    //  } else if( i % 5 === 0 ) {
-        
-    //      console.log("Buzz");
-
-    //  } 
     
-    //  if ( ( i % 3 === 0 ) && ( i % 5 === 0 ) ){
 
-    //      console.log("FizzBuzz");
-
-    //  }
-
-    //  TENTATIVO2 => FAILED
-
-    // if ( ( i % 3 === 0 ) || ( i % 5 === 0 ) )  {
-
-    //     if ( i % 3 === 0 ) {
-
-    //         console.log( "Fizz" );
-
-    //     } else if ( i % 5 === 0 ) {
-
-    //         console.log( "Buzz" );
-    //     }
-
-    // } else if ( ( i % 3 === 0 ) && ( i % 5 === 0 ) ) {
-        
-    //     console.log( "FizzBuzz" );
    
-    // }
 
-    //  TENTATIVO3: FAILED
-    // if ( i % 3 === 0 ) {
+    //   TENTATIVO3: FAILED
+     if ( i % 15 === 0 ) {
 
-    //     console.log( "Fizz" );
+         console.log( "FizzBuzz" );
+         containerBuzzEl.innerHTML += `<div class="square-container"> FizzBuzz </div>`;
+          
 
-    //  } else if( i % 5 === 0 ) {
-        
-    //     console.log( "Buzz" );
+      } else if( i % 5 === 0 ) {
+      
+         console.log( "Buzz" );
+         containerBuzzEl.innerHTML += `<div class="square-container"> Buzz </div>`;
+         
 
-    //  } else if ( i % 15 === 0 ) {
+      } else if ( i % 3 === 0 ) {
 
-    //     console.log( "FizzBuzz" )
+         console.log( "Fizz" );
 
-    //  }
+         containerBuzzEl.innerHTML += `<div class="square-container"> Fizz </div>`;
 
-    //  TENTATIVO4: FAILED
-    if ( ( i % 3 === 0 ) || ( i % 5 === 0 ) )  {
-        if ( i % 3 === 0 ) {
-            console.log( "Fizz" );   
-        } else if ( i % 5 === 0 ) {
-            console.log( "Buzz" );
-        }
-    } else if (( i % 15 === 0 )) {
-        console.log( "FizzBuzz" )
-    }
+      }
+
 }

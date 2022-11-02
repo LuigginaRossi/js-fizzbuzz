3dd650d0e88857c8d7e21f2cb1cd12283acebbe9
LuigginaRossi <luigginavaldivia@gmail.com>
2022-11-02 17:15:55 +0100

    4 tentativo

--- js/file.js
+++ js/file.js
@@ -8,37 +8,66 @@ for ( let i = 0; i <= 100; i++ ) {
     containerBuzzEl.innerHTML += `<div class="square-container"> ${i} </div>`;
     console.log(containerBuzzEl);
 
+    //     TENTATIVO1=> FAILED
+   
     // if ( i % 3 === 0 ) {
 
-    //     
+    //     console.log( "Fizz" );
 
-    // } else if( i % 5 === 0 ) {
+    //  } else if( i % 5 === 0 ) {
         
-    //     console.log("Buzz");
+    //      console.log("Buzz");
 
-    // } 
+    //  } 
     
-    // if ( ( i % 3 === 0 ) && ( i % 5 === 0 ) ){
+    //  if ( ( i % 3 === 0 ) && ( i % 5 === 0 ) ){
 
-    //     console.log("FizzBuzz");
+    //      console.log("FizzBuzz");
 
+    //  }
+
+    //  TENTATIVO2 => FAILED
+
+    // if ( ( i % 3 === 0 ) || ( i % 5 === 0 ) )  {
+
+    //     if ( i % 3 === 0 ) {
+
+    //         console.log( "Fizz" );
+
+    //     } else if ( i % 5 === 0 ) {
+
+    //         console.log( "Buzz" );
+    //     }
+
+    // } else if ( ( i % 3 === 0 ) && ( i % 5 === 0 ) ) {
+        
+    //     console.log( "FizzBuzz" );
+   
     // }
 
+    //  TENTATIVO3: FAILED
+    // if ( i % 3 === 0 ) {
+
+    //     console.log( "Fizz" );
+
+    //  } else if( i % 5 === 0 ) {
+        
+    //     console.log( "Buzz" );
+
+    //  } else if ( i % 15 === 0 ) {
+
+    //     console.log( "FizzBuzz" )
+
+    //  }
+
+    //  TENTATIVO4: FAILED
     if ( ( i % 3 === 0 ) || ( i % 5 === 0 ) )  {
-
         if ( i % 3 === 0 ) {
-
-            console.log( "Fizz" );
-
+            console.log( "Fizz" );   
         } else if ( i % 5 === 0 ) {
-
             console.log( "Buzz" );
         }
-
-    } else if ( ( i % 3 === 0 ) && ( i % 5 === 0 ) ) {
-        
-        console.log( "FizzBuzz" );
-   
+    } else if (( i % 15 === 0 )) {
+        console.log( "FizzBuzz" )
     }
 }
-

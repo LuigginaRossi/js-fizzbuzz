d2938f16903b87b9d385833ddd1b2814ae60ccda
LuigginaRossi <luigginavaldivia@gmail.com>
2022-11-02 16:34:04 +0100

    aggiunta tentativo condizione

--- js/file.js
+++ js/file.js
@@ -3,7 +3,24 @@ const containerBuzzEl = document.getElementById( "container-buzz" );
 console.log( containerBuzzEl );
 
 for ( let i = 0; i <= 100; i++ ) {
+
     console.log(i);
-    containerBuzzEl.innerHTML = `<div class="square-container"> ${i} </div>`;
+    containerBuzzEl.innerHTML += `<div class="square-container"> ${i} </div>`;
     console.log(containerBuzzEl);
+
+    if ( i % 3 === 0 ) {
+
+        console.log("Fizz");
+
+    } else if( i % 5 === 0 ) {
+        
+        console.log("Buzz");
+
+    } 
+    
+    if ( ( i % 3 === 0 ) && ( i % 5 === 0 ) ){
+
+        console.log("FizzBuzz");
+
+    }
 }
